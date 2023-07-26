504e2191c56ad817fad37d325f568b84bf8ffe55
Tyler Wagner <tylerrwagner38@gmail.com>
2023-07-26 11:22:13 -0500

    Updated the learning tree
    Updated the entire learning tree. Working on the next part of the tree which is the best split. That is where errors are residing currently

--- HTLearning.py
+++ HTLearning.py
@@ -1,12 +1,13 @@
 """
 Author: Hayden Marshall
 Date Created: 7/24/23
-Edited by: Hayden Marshall
-Date Edited: 7/24/23
+Edited by: Hayden Marshall and Tyler Wagner
+Date Edited: 7/26/23
 """
 
 import numpy as np
 from collections import Counter
+from math import log2
 
 class Node:
     def __init__(self, examples, children, attribute, depth):
@@ -23,17 +24,16 @@ class Node:
         return False
     
 class DecisionTree:
-    def __init__(self, root_node = None, max_depth=None, num_attributes = None, min_num_examples = 2):
-        self.root_node = root_node
+    def __init__(self, max_depth=None, num_attributes = None, min_num_examples = 2):
         self.max_depth = max_depth
         self.num_attributes = num_attributes
         self.min_num_examples = min_num_examples
+        self.root_node = None
 
     def fit(self, training, testing):
         self.num_attributes = training.shape[1] if not self.num_attributes else min(self.num_attributes, training.shape[1])
         
-
-        self.root = self.create_tree(training, testing)
+        self.root_node = self.create_tree(training, testing)
 
     def create_tree(self, training, testing, depth=0):
         num_examples, num_attr = training.shape
@@ -47,31 +47,91 @@ class DecisionTree:
         attribute_indexes = np.random.choice(num_attr, self.num_attributes, replace=False) #making sure we choose based on best information gain and not randomly
 
         #finding the best split
-        best_attribute = self._best_split(training, testing, attribute_indexes)
+        best_attribute, best_thresh = self._best_split(training, testing, attribute_indexes)
 
         #create the child nodes
+        left_indexes, right_indexes = self._split(training[:, best_attribute], best_thresh)
+        left = self.create_tree(training[left_indexes, :],training[left_indexes], depth + 1)
+        right = self.create_tree(testing[right_indexes, :], training[right_indexes], depth + 1)
+        return Node(best_attribute, best_thresh, left, right)
 
 
 
     def _most_common_label(self, testing):
-        counter = Counter(testing)
-        value = counter.most_common(1)[0][1]
+        value = -1
+        for column_name in testing:
+            if testing[column_name].value_counts().nlargest(1).values[0] > value:
+                value = testing[column_name].value_counts().nlargest(1).values[0]
+
         return value
     
 
-    def entropy(self, attribute_values):
-        hist = np.bincount(attribute_values)
-
-        qs = hist / len(attribute_values)
-
-        return -np.sum(q * np.log2(q) for q in qs if q > 0)
+    def _entropy(self, attribute_values):
+        labels, values = np.unique(attribute_values, return_counts=True)
+        qs = values / len(attribute_values)
+        entropy = -np.sum([q * np.log(q)for q in qs if q > 0])
+        return entropy
     
-    def information_gain(self):
+    def _information_gain(self, testing, training_column, thresh):
 
-        parent_entropy = self.entropy()
+        parent_entropy = self._entropy(testing)
+        print("Parent entropy: ")
+        print(parent_entropy)
 
-    def split():
-        return True
+        left_indexes, right_indexes = self._split(training_column, thresh)
+        print("Left indexes:")
+        print(left_indexes)
+
+        if len(left_indexes) == 0 or len(right_indexes) == 0:
+            return 0
+        
+        total_num = len(testing)
+        num_left, num_right = len(left_indexes), len(right_indexes)
+        examples_left, examples_right = self._entropy(testing[left_indexes]), self._entropy(testing[right_indexes])
+        child_entropy = (num_left / total_num) * examples_left + (num_right / total_num) * examples_right
+        print("child entropy:")
+        print(child_entropy)
+
+        information_gain = parent_entropy - child_entropy
+        print(information_gain)
+        return information_gain
+                
+
+
+    def _split(self, training_column, split_threshold):
+        left_indexes = np.argwhere(training_column <= split_threshold).flatten()
+        right_indexes = np.argmax(training_column > split_threshold).flatten()
+        return left_indexes, right_indexes
 
     def _best_split(self, training, testing, attribute_indexes):
-        return True
+        best_gain = -1
+        split_index, split_threshold = None
+
+        for attribute_index in attribute_indexes:
+            training_column = training[:, attribute_index]
+            thresholds = np.unique(training_column)
+
+            for thr in thresholds:
+                #calculate information gain
+                gain = self._information_gain(training, training_column) #CHECK HERE FOR INFORMATION GAIN ERRORS
+
+                if gain > best_gain:
+                    best_gain = gain
+                    split_index = attribute_index
+                    split_threshold = thr
+
+            return split_index, split_threshold
+    
+
+    
+    def predict(self, training):
+        return np.array([self._traverse_tree(x) for x in training])
+    
+    def _traverse_tree(self, x, node):
+        if node.is_leaf_node():
+            return node.value
+        
+        if x[node.feature] <= node.threshold:
+            return self._traverse_tree(x ,node.left)
+        
+        return self._traverse_tree(x, node.right)
--- Main.py
+++ Main.py
@@ -6,39 +6,31 @@ Edited Date: 7/25/23
 """
 
 import csv
-from tabulate import tabulate #used to display the 2D array
 from GUI import window
 from HTLearning import DecisionTree, Node
 import numpy as np
 import pandas as pd
 
-#allows the user to read a csv file and import it as a 2D array
-def read_csv_file(file_path):
-    CSVData = []
-    with open(file_path, newline='') as csvfile:
-        csv_reader = csv.reader(csvfile)
-        for row in csv_reader:
-            CSVData.append(row)
-    return CSVData
-
-#displays the table (ONLY USE IF YOU THINK THERE IS AN ERROR IN THE TABLE)
-def display_array(FilledCSVData):
-    myheaders = FilledCSVData[0]
-    data = FilledCSVData[1:]
-    print(tabulate(data, headers=myheaders, tablefmt = "simple"))
 
 def main():
-    
-    tree = DecisionTree(max_depth=10) #change max depth to be a user input
+    user_in_max_depth = int(input("Enter the max depth you want the tree to have:"))
+
+    tree = DecisionTree(max_depth=user_in_max_depth) #change max depth to be a user input
     training_rest_data = pd.read_csv("restaurant.csv")
     training_rest_data.columns = ["Alt", "Bar", "Friday?", "Hungry?", "NumPatrons", "Price", "Rain?", "Res", "Type", "Est wait Time", "(Output)WillWait"]
     testing_rest_data = pd.read_csv("restaurant_test.csv")
     testing_rest_data.columns = ["Alt", "Bar", "Friday?", "Hungry?", "NumPatrons", "Price", "Rain?", "Res", "Type", "Est wait Time", "(Output)WillWait"]
-    #print(tree._most_common_label(training_rest_data))
+    
+    tree.fit(training_rest_data, testing_rest_data)
 
-    #error checking area
+
+    #ERROR TESTING
+    # print(training_rest_data)
+    # MCL = tree._most_common_label(training_rest_data)
+    # print(MCL)
     #print(training_rest_data.isnull().sum())
-    print(training_rest_data.dtypes)
+    # entropy = tree.entropy(training_rest_data["Alt"])
+    # print(entropy)
 
 
 if __name__ == "__main__":
